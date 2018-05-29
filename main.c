bf50ed14c6bbd18f1ab69dbcc7fb96a17bdb4d7b
Brian Sanchez <briansanchezizq@gmail.com>
2018-05-28 23:19:11 -0500

    Update main.c

--- main.c
+++ main.c
@@ -5,15 +5,35 @@ int main()
     char    nombre[50];
     char    apellido[50];
     char    copia[50];
-    char    string4;
-    int     i, largo;
+    char    resp;
+    int     i, largo, lrgcrtr;
     printf("\nEscriba su nombre: ");
     scanf("%s", nombre);
     printf("Escriba su apellido: ");
-	scanf("%s", apellido);
+    scanf("%s", apellido);
     strcat(nombre, " ");
     strcat(nombre, apellido);
     printf("\t\t%s",nombre);
+	printf("Es correcta la informacion? (s/n): ");
+	scanf("%c", resp);
+	switch(resp)
+	{
+		case 'y':	
+			break;
+		case 'n':
+			largo = strlng(nombre);
+			printf("\nSu nombre tiene %d caracteres de longitud.\nCuantos caracteres desea modificar desea modificar?: ",lrgcrtr);
+			scanf("%i", &lrgcrtr);
+			printf("\n");
+			for(i=0;i<lgrcrtr:i++)
+			{
+				printf("%i",i);
+			}
+			break;
+		default:
+			break;
+	}
+	
     /*
     strcpy(copia, nombre);
     i = strcmp(copia, nombre);
