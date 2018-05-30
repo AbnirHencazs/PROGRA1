47d8d8fa42ac77e5d8932f3fe16d3f58f157d799
Brian Sanchez <briansanchezizq@gmail.com>
2018-05-30 08:54:38 -0500

    ACTUALIZACION A OPERACIONES DE MATRICES DINAMICAS.ver3
    En esta versión ya incluye la potencia de las matrices.

--- main.c
+++ main.c
@@ -8,8 +8,8 @@
      int    i,j,k;
      int    mtrxa[MAX_MTRX][MAX_MTRX];
      int    mtrxb[MAX_MTRX][MAX_MTRX];
-     int    mtrxres[MAX_MTRX][MAX_MTRX];
-     char   Op, siono;
+     int    mtrxres[MAX_MTRX][MAX_MTRX]={0,0};
+     char   Op, siono, mtx;
      //A
      printf("Numero de columnas de la matriz 'A': ");
      scanf("%d",&col_A);
@@ -24,6 +24,21 @@
              scanf("%d", &mtrxa[i][j]);
              }
      }
+     //B
+     printf("Numero de columnas de la matriz 'B': ");
+     scanf("%d",&col_B);
+     printf("Numero de filas de la matriz 'B': ");
+     scanf("%d",&fil_B);
+     
+     for(i=0;i<fil_B;i++)
+     {
+         for(j=0;j<col_B;j++)
+         {
+             printf("Ingrese el valor de la posicion %d,%d: ",i,j);
+             scanf("%d", &mtrxb[i][j]);
+             }
+     }
+     //IMPRESION
      printf("A:\n");
      for(i=0;i<fil_A;i++)
      {
@@ -39,20 +54,6 @@
                  {
                      printf("\n");
                      }
-     }
-     //B
-     printf("Numero de columnas de la matriz 'B': ");
-     scanf("%d",&col_B);
-     printf("Numero de filas de la matriz 'B': ");
-     scanf("%d",&fil_B);
-     
-     for(i=0;i<fil_B;i++)
-     {
-         for(j=0;j<col_B;j++)
-         {
-             printf("Ingrese el valor de la posicion %d,%d: ",i,j);
-             scanf("%d", &mtrxb[i][j]);
-             }
      }
      printf("B:\n");
      for(i=0;i<fil_B;i++)
@@ -111,8 +112,94 @@
                    printf("\nOperacion invalida, revise las dimensiones de las matrices.");
                    }
             break;
+            
             case '^':
+            printf("Que matriz desea elevar a la segunda potencia?: ");
+            scanf("%c",&mtx);
+            scanf("%c",&mtx);
+            switch(mtx)
+            {
+                case 'A':
+                if(col_A==fil_A)
+                {
+                    fil_S = fil_A;
+                    col_S = col_A;
+                    for(i=0;i<fil_S;i++)
+                    {
+                        for(j=0;j<col_S;j++)
+                        {
+                            //mtrxres[i][j]=0;
+                            for(k=0;k<col_A;k++)
+                            {
+                                mtrxres[i][j]+=mtrxa[i][k]*mtrxa[k][j];
+                                }
+                            }
+                        }
+                        printf("RESULTADO:\n");
+                         for(i=0;i<fil_S;i++)
+                         {
+                             for(j=0;j<col_S;j++)
+                             {
+                                 printf("\t%i\t",mtrxres[i][j]);
+                                 }
+                                 if(i==0)
+                                 {
+                                     printf("\n");
+                                     }
+                                     if(i>0)
+                                     {
+                                         printf("\n");
+                                         }
+                            }
+                    }
+                    else
+                        {
+                            printf("La potencia solo se da en Matrices cuadradas, porfavor reingrese su Matriz.");
+                            }
+                break;
+                case 'B':
+                if(col_B==fil_B)
+                {
+                    fil_S = fil_B;
+                    col_S = col_B;
+                    for(i=0;i<fil_S;i++)
+                    {
+                        for(j=0;j<col_S;j++)
+                        {
+                            //mtrxres[i][j]=0;
+                            for(k=0;k<col_B;k++)
+                            {
+                                mtrxres[i][j]+=mtrxb[i][k]*mtrxb[k][j];
+                                }
+                            }
+                        }
+                        printf("RESULTADO:\n");
+                         for(i=0;i<fil_S;i++)
+                         {
+                             for(j=0;j<col_S;j++)
+                             {
+                                 printf("\t%i\t",mtrxres[i][j]);
+                                 }
+                                 if(i==0)
+                                 {
+                                     printf("\n");
+                                     }
+                                     if(i>0)
+                                     {
+                                         printf("\n");
+                                         }
+                            }
+                    }
+                    else
+                        {
+                            printf("La potencia solo se da en Matrices cuadradas, porfavor reingrese su Matriz.");
+                            }
+                break;
+                default:
+                break;
+                }
             break;
+            
             case '*':
             if(col_A == fil_B)
             {
@@ -122,7 +209,6 @@
                 {
                     for(j=0;j<col_S;j++)
                     {
-                        mtrxres[i][j]=0;
                         for(k=0;k<col_A;k++)
                         {
                             mtrxres[i][j]+=mtrxa[i][k]*mtrxb[k][j];
